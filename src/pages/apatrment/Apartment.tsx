9d0e04bdd5dfa7be38d77f15ac703c0ba0e198b6
Danylo Dykhno <dykhnodanylo10@gmail.com>
2023-07-28 20:14:54 +0300

    func change

--- src/pages/apatrment/Apartment.tsx
+++ src/pages/apatrment/Apartment.tsx
@@ -55,7 +55,7 @@ function Apartment() {
     setOpenAdd(false)
   }, [resultAddOrder.isSuccess])
 
-  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+  const handleClose = (_event?: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === 'clickaway') {
       return;
     }
